4ec0af10304a8a31ef92153d0fe76ade7fa58160
root <root@ansible.reitek.com>
2018-10-22 17:14:41 +0200

    Commit iniziale

---
+++ README.md
@@ -0,0 +1,30 @@
+Descrizione Role
+=========
+
+Modifica il portale delle "chiamate mute" (portale statistiche delle chiamate abbattute alla risposta)
+
+Requisiti
+------------
+
+Nessuno
+
+Variabili
+--------------
+
+Nessuna
+
+Dipendenze
+------------
+
+Nessuna
+
+Esempio di chiamata
+----------------
+
+ansible-playbook /home/playbook/bugs/6533/main.yml
+
+Informazioni Autore
+------------------
+
+Martino.Viganò
+Martino.Vigano@enghouse.com
---
+++ files/_header.php
@@ -0,0 +1,19 @@
+<?php
+date_default_timezone_set("Europe/Rome");
+?>
+<html>
+<head>
+ <title>Gestione Download</title>
+  <link rel="stylesheet" href="jquery-ui/css/smoothness/jquery-ui.css">
+  <link href="dl.css" type="text/css" rel="stylesheet">
+  <script src='js/jquery.js' language='javascript'></script>
+  <script src='js/jquery-ui.js' language='javascript'></script>
+</head>
+<body> 
+ <div id='wrapper' class='wrapper'>
+  <div class='header'>
+   <div class='header_top'>
+    <span class='titolo'>Silent Calls Reports<br/>Download</span>
+     <img src='img/EnghouseLogo.png'>
+   </div>
+  </div>
